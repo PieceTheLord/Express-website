0187a897c3888539f35f9d9f53316408adffa292
PieceTheLord <alwx202@mail.ru>
2023-12-09 18:57:27 +0300

    begin of the server

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,59 +1,14 @@
-const http = require('node:http');
-const fs = require('fs')
+const express = require('express')
+const path = require('path')
+const app = express()
 
-const hostname = 'localhost';
+// const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+app.use(express.static(path.join('G:/IT/JavaScriptTrain/ExploreNodeJs/SecondProject/frontEnd', 'public')))
 
-  // set header content type
-  res.setHeader("Content-Type", 'text/html')
+app.get('/', (req, res) => {
+  res.render('G:/IT/JavaScriptTrain/ExploreNodeJs/SecondProject/frontEnd/views/')
+})
 
-  let path = '../frontEnd/views/';
-  switch (req.url) {
-    case '/':
-      path += 'index.html';
-      res.statusCode = 200;
-      break;
-  
-    case '/profile':
-      path += 'profileUser.html';
-      res.statusCode = 200;
-      break;
-  
-    // case '/about-Me':
-    //   res.statusCode = 301;
-    //   res.setHeader("Location", '/profile')
-    //   res.end();
-    //   break;
-
-    case '/authentication':
-      path += 'authentication.html';
-      res.statusCode = 200;
-      break;
-  
-    default:
-      path += 'Error404.htm';
-      res.statusCode = 404;
-      break;
-  }
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log('Happened an error');
-      res.end()
-    } else{
-      res.end(data)
-    }
-  })
-
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-
-
-
+app.listen(port)

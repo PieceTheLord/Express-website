0f2738d7e971375f80a2598712640d5538b855ec
PieceTheLord <alwx202@mail.ru>
2023-12-15 00:04:49 +0300

    some changes inside the design and not finished authentication

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const flash = require('express-flash');
+const expressSession = require('express-session');
 const path = require('path')
 
 const app = express()
@@ -8,6 +10,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(pathWork, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressSession({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,    
+}));
+app.use(flash());
 
 app.get('/', (req, res) => {
     res.render(pathWork = 'G:/IT/JavaScriptTrain/ExploreNodeJs/SecondProject/frontEnd/views/home.ejs')
